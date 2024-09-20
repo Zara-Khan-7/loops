254d391c78f2537cc44b662f87945bd6312d91d2
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-09-21 01:53:03 +0500

    Add files via upload

---
+++ class10loops/main.js
@@ -0,0 +1,58 @@
+"use strict";
+// LOOPS(FOR)
+// for(let i=0; true; i++){
+//     console.log('hello world')
+// };
+// for(let i=1; i<=5; i++){
+//     console.log('hellow world')
+// }
+// for(let i=1; i<10; i=i+2){
+//     console.log('hello world')
+// }
+// for(let i=0; i<55; i++){
+//     console.log(`${i} hello world`)
+// };
+// for(let i=10; i>=1; i--){
+//     console.log(`${i} hello world`)
+// }
+// let car=["mehran","corolla","civic","alto"]
+// for (let i=0; i<car.length; i++){
+//     console.log(car[i])
+// }
+// let makeup=["Missrose","Mac","Etude","Hudabeauty","loreal"]
+// for (let z=0; z<2; z++){
+//     console.log(makeup[z])
+//     console.log(makeup.length)
+// }
+// let makeup1=["Missrose","Mac","Etude","Hudabeauty","loreal"]
+// for (let z=0; z<=2; z++){
+//     console.log(makeup[z])
+// }
+// let makeup2=["Missrose","Mac","Etude","Hudabeauty","loreal"]
+// for (let z=0; z<makeup2.length; z++){
+//     console.log(makeup2[z])
+// }
+// let makeup3:string[]=["Missrose","Mac","Etude","Hudabeauty","loreal"]
+// for (let z=0; z<4; z++){
+//     console.log(makeup3[z])
+// }
+// INFINITE LOOP
+// for (let i=0;true;i++){
+//     console.log(i)
+// };
+// for (let z=1; true; z++){
+//     console.log(z)
+// };
+// for (let i=0 ; i<5 ; i++){
+//     console.log('hello zara')
+// };
+// for (let i=5 ; i<=20 ; i++){
+//     console.log(`${i}hello zara`)
+// };
+// for (let i=0 ; i<=5 ; i+=2 ){
+//     console.log(`${i}hello zara`)
+// };
+for (let i = 0; i <= 20; i += 4) {
+    console.log(`${i}hello zara`);
+}
+;
